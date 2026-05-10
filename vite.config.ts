5396b928769c52a1f6df50f104a649294bb9410e
razhanhamzarasul-design <razhan.hamza.rasul@gmail.com>
2026-05-11 01:45:36 +0300

    Set base path and add React plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -34,3 +34,7 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+export default defineConfig({
+  base: "/Premiumrestaurantmenuui/",
+  plugins: [react()],
+})
